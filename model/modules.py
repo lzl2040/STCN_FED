f48842bce4f78dafc48d9b542c46781e02890263
lzl2040 <2040171586@qq.com>
2023-02-26 20:57:01 +0800

    Initial commit

---
+++ model/modules.py
@@ -0,0 +1,271 @@
+"""
+modules.py - This file stores the rathering boring network blocks.
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+
+from model import mod_resnet
+from model import cbam
+import numpy as np
+
+class ResBlock(nn.Module):
+    def __init__(self, indim, outdim=None):
+        super(ResBlock, self).__init__()
+        if outdim == None:
+            outdim = indim
+        if indim == outdim:
+            self.downsample = None
+        else:
+            self.downsample = nn.Conv2d(indim, outdim, kernel_size=3, padding=1)
+ 
+        self.conv1 = nn.Conv2d(indim, outdim, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(outdim, outdim, kernel_size=3, padding=1)
+ 
+    def forward(self, x):
+        r = self.conv1(F.relu(x))
+        r = self.conv2(F.relu(r))
+        
+        if self.downsample is not None:
+            x = self.downsample(x)
+
+        return x + r
+
+
+class FeatureFusionBlock(nn.Module):
+    def __init__(self, indim, outdim):
+        super().__init__()
+
+        self.block1 = ResBlock(indim, outdim)
+        self.attention = cbam.CBAM(outdim)
+        self.block2 = ResBlock(outdim, outdim)
+
+    def forward(self, x, f16):
+        x = torch.cat([x, f16], 1)
+        x = self.block1(x)
+        r = self.attention(x)
+        x = self.block2(x + r)
+
+        return x
+
+
+# Single object version, used only in static image pretraining
+# This will be loaded and modified into the multiple objects version later (in stage 1/2/3)
+# See model.py (load_network) for the modification procedure
+class ValueEncoderSO(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        resnet = mod_resnet.resnet18(pretrained=True, extra_chan=1)
+        self.conv1 = resnet.conv1
+        self.bn1 = resnet.bn1
+        self.relu = resnet.relu  # 1/2, 64
+        self.maxpool = resnet.maxpool
+
+        self.layer1 = resnet.layer1 # 1/4, 64
+        self.layer2 = resnet.layer2 # 1/8, 128
+        self.layer3 = resnet.layer3 # 1/16, 256
+
+        self.fuser = FeatureFusionBlock(1024 + 256, 512)
+
+    def forward(self, image, key_f16, mask):
+        # key_f16 is the feature from the key encoder
+
+        f = torch.cat([image, mask], 1)
+
+        x = self.conv1(f)
+        x = self.bn1(x)
+        x = self.relu(x)   # 1/2, 64
+        x = self.maxpool(x)  # 1/4, 64
+        x = self.layer1(x)   # 1/4, 64
+        x = self.layer2(x) # 1/8, 128
+        x = self.layer3(x) # 1/16, 256
+
+        x = self.fuser(x, key_f16)
+
+        return x
+
+
+# Multiple objects version, used in other times
+class ValueEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        resnet = mod_resnet.resnet18(pretrained=True, extra_chan=2)
+        self.conv1 = resnet.conv1
+        self.bn1 = resnet.bn1
+        self.relu = resnet.relu  # 1/2, 64
+        self.maxpool = resnet.maxpool
+
+        self.layer1 = resnet.layer1 # 1/4, 64
+        self.layer2 = resnet.layer2 # 1/8, 128
+        self.layer3 = resnet.layer3 # 1/16, 256
+
+        self.fuser = FeatureFusionBlock(1024 + 256, 512)
+
+    def forward(self, image, key_f16, mask, other_masks):
+        # key_f16 is the feature from the key encoder
+
+        f = torch.cat([image, mask, other_masks], 1)
+
+        x = self.conv1(f)
+        x = self.bn1(x)
+        x = self.relu(x)   # 1/2, 64
+        x = self.maxpool(x)  # 1/4, 64
+        x = self.layer1(x)   # 1/4, 64
+        x = self.layer2(x) # 1/8, 128
+        x = self.layer3(x) # 1/16, 256
+
+        x = self.fuser(x, key_f16)
+
+        return x
+ 
+
+class KeyEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        resnet = models.resnet50(pretrained=True)
+        self.conv1 = resnet.conv1
+        self.bn1 = resnet.bn1
+        self.relu = resnet.relu  # 1/2, 64
+        self.maxpool = resnet.maxpool
+
+        self.res2 = resnet.layer1 # 1/4, 256
+        self.layer2 = resnet.layer2 # 1/8, 512
+        self.layer3 = resnet.layer3 # 1/16, 1024
+
+    def forward(self, f):
+        x = self.conv1(f) 
+        x = self.bn1(x)
+        x = self.relu(x)   # 1/2, 64
+        x = self.maxpool(x)  # 1/4, 64
+        f4 = self.res2(x)   # 1/4, 256
+        f8 = self.layer2(f4) # 1/8, 512
+        f16 = self.layer3(f8) # 1/16, 1024
+
+        return f16, f8, f4
+
+
+class UpsampleBlock(nn.Module):
+    def __init__(self, skip_c, up_c, out_c, scale_factor=2):
+        super().__init__()
+        self.skip_conv = nn.Conv2d(skip_c, up_c, kernel_size=3, padding=1)
+        self.out_conv = ResBlock(up_c, out_c)
+        self.scale_factor = scale_factor
+
+    def forward(self, skip_f, up_f):
+        x = self.skip_conv(skip_f)
+        x = x + F.interpolate(up_f, scale_factor=self.scale_factor, mode='bilinear', align_corners=False)
+        x = self.out_conv(x)
+        return x
+
+
+class KeyProjection(nn.Module):
+    def __init__(self, indim, keydim):
+        super().__init__()
+        self.key_proj = nn.Conv2d(indim, keydim, kernel_size=3, padding=1)
+        # shrinkage
+        # self.d_proj = nn.Conv2d(indim, 1, kernel_size=3, padding=1)
+        # # selection
+        # self.e_proj = nn.Conv2d(indim, keydim, kernel_size=3, padding=1)
+
+        nn.init.orthogonal_(self.key_proj.weight.data)
+        nn.init.zeros_(self.key_proj.bias.data)
+    
+    def forward(self, x):
+        return self.key_proj(x)
+
+
+def get_frequency_modes(seq_len, modes=64, mode_select_method='random'):
+    """
+    get modes on frequency domain:
+    'random' means sampling randomly;
+    'else' means sampling the lowest modes;
+    """
+    modes = min(modes, seq_len//2)
+    if mode_select_method == 'random':
+        index = list(range(0, seq_len // 2))
+        np.random.shuffle(index)
+        index = index[:modes]
+    else:
+        index = list(range(0, modes))
+    index.sort()
+    return index
+
+# ########## fourier layer #############
+class FourierBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, modes=0, mode_select_method='random'):
+        super(FourierBlock, self).__init__()
+        print('fourier enhanced block used!')
+        """
+        1D Fourier block. It performs representation learning on frequency domain, 
+        it does FFT, linear transform, and Inverse FFT.    
+        """
+        # get modes on frequency domain
+        self.heads = 8
+        self.modes = modes
+        self.mode_select_method = mode_select_method
+        self.index = None
+        # print('modes={}, index={}'.format(modes, self.index))
+
+        self.scale = (1 / (in_channels * out_channels))
+        # parameterized kernel:D * D * M
+        # self.weights1 = nn.Parameter(self.scale * torch.rand(8, in_channels // 8, out_channels // 8, len(self.index),
+        #                                 dtype=torch.cfloat))
+        # 实部,8为head num
+        self.weights1_real = nn.Parameter(
+            self.scale * torch.rand(8, in_channels // 8, out_channels // 8, modes,1,
+                                        dtype=torch.float))
+        # 虚部,8为head num
+        self.weights1_img = nn.Parameter(
+            self.scale * torch.rand(8, in_channels // 8, out_channels // 8, modes,1,
+                                    dtype=torch.float))
+        # 拼接起来
+        # self.weight1 = torch.cat([self.weight1_real,self.weight1_img],dim=0)
+        # print('weight shape:' + str(self.weights1.shape))
+        # print(self.weights1)
+
+    # Complex multiplication
+    def compl_mul1d(self, input, weights_real,weights_img):
+        # (batch, in_channel, x ), (in_channel, out_channel, x) -> (batch, out_channel, x)
+        # 手动实现复数运算
+        weights = torch.cat([weights_real,weights_img],dim=-1)
+        weights = torch.view_as_complex(weights)
+        # print('weights shape:' + str(weights.shape))
+        # print('input shape:' + str(input.shape))
+        return torch.einsum("bhi,hio->bho", input, weights)
+
+    def forward(self, feature):
+        # print(feature)
+        B,C,H,W = feature.shape
+        # 先变成B,C,L吧
+        feature = feature.view(B,C,H * W).transpose(1,2)
+        feature = feature.view(B,H * W,self.heads,-1)
+        # print('feature shape:' + str(feature.shape))
+        # 对feature进行映射
+
+        # size = [B, L, H, E] H:head E:channel
+        B, L, He, E = feature.shape
+        if self.index == None:
+            self.index = get_frequency_modes(L, modes=self.modes, mode_select_method=self.mode_select_method)
+            print('modes={}, index={}'.format(self.modes, self.index))
+        x = feature.permute(0, 2, 3, 1)
+        # print('x shape:' + str(x.shape))
+        # Compute Fourier coefficients
+        # x:B He E L
+        x_ft = torch.fft.rfft(x)
+        # print('x_ft shape:' + str(x_ft.shape))
+        # Perform Fourier neural operations
+        out_ft = torch.zeros(B, He, E, L // 2 + 1, device=x.device, dtype=torch.cfloat)
+        for wi, i in enumerate(self.index):
+            out_ft[:, :, :, wi] = self.compl_mul1d(x_ft[:, :, :, i], self.weights1_real[:, :, :, wi,:],
+                                                   self.weights1_img[:,:,:,wi,:])
+        # Return to time domain
+        x = torch.fft.irfft(out_ft, n=x.size(-1))
+        # print('final x shape:' + str(x.shape))
+        # 从B H E L恢复成B C H W
+        x = x.view(B,He * E,H,W)
+        # print('final x shape:' + str(x.shape))
+        return x
